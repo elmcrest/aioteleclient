dbda9b206333cbc846f0da2b5d68283b8455bd48
Marius Räsener <m.raesener@gmail.com>
2018-03-21 15:51:52 +0100

    setup travis with coverage

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='async-telegram-client',
+    name='aioteleclient',
     version='0.0.1',
-    url='https://github.com/elmcrest/async-telegram-client',
+    url='https://github.com/elmcrest/aioteleclient',
     license='MIT',
     description='An fast and heavily tested async telegram messenger client.',
     long_description="An fast and heavily tested async telegram messenger client",
     author='Marius Räsener',
     author_email='m.raesener@gmail.com',
-    keywords='python telegram-client telegram-client-api asyncio async-telegram-client bot bot-framework telegram',
+    keywords='python telegram-client telegram-client-api asyncio async-telegram-client'
+             'bot bot-framework telegram aioteleclient',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Topic :: Communications :: Chat'
     ],
+    packages=[
+        'aioteleclient.'+package for package in
+        find_packages('aioteleclient')
+    ],
     install_requires=[
         'aiohttp'
     ],
-
 )
